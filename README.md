1d886edd106d63f0222cde539e57a80200fad9c3
Liuyanfeng <lyf571321556@163.com>
2023-10-17 16:17:15 +0800

    add: 自动配置flutter sdk支持chrome调试

--- README.md
+++ README.md
@@ -6,10 +6,16 @@
 * cd bin ,执行 dart compile exe dart_proxy_server.dart -o dart-proxy-server-intel-mac，编译可执行程序
   *目前不支持交叉编译，因此需要在对应平台运行该编译器。
 
-# example
+# 启动顺序说明
 
-[root@master ~]# ./dart-proxy-server-intel-mac -t https://appdev.myones.net -p 3000
+* 先启动本地代理服务
 
-```
-https://appdev.myones.net 的本地代理地址是 http://localhost:3000
+      [root@master ~]# ./dart-proxy-server-intel-mac -t https://appdev.myones.net -p 3000
+
+        https://appdev.myones.net 的本地代理地址是 http://localhost:3000
+
+* 再启动flutter web服务
+
+      [root@master ~]#  flutter run -d chrome --web-renderer html -t lib/main_dev.dart
+
  
--- bin/dart_proxy_server.dart
+++ bin/dart_proxy_server.dart
@@ -11,23 +11,123 @@ import 'package:args/args.dart';
 
 const String LocalHost = 'localhost';
 const int LocalPort = 3000;
-const String TargetUrl = 'https://api.ones.cn/';
+const String TargetUrl = 'https://ones.cn/';
 
 void main(List<String> arguments) async {
-  final argParser = ArgParser()
-    ..addOption('target', abbr: 't', defaultsTo: TargetUrl)
-    ..addOption('port', abbr: 'p', defaultsTo: LocalPort.toString());
-  final argResults = argParser.parse(arguments);
-  final String targetUrl = argResults['target'];
-  final port = int.parse(argResults['port']);
-  final server = await shelf_io.serve(
-    // proxyHandler(TargetUrl),
-    customProxyHandler(targetUrl),
-    LocalHost,
-    port,
-  );
-  configServer(server);
-  print('$targetUrl 的本地代理地址是 http://${server.address.host}:${server.port}');
+  await deleteFlutterWebToolsStamp()
+      .then((value) => disableFlutterWebSecurity())
+      .then((value) => reBuildFlutterTool(value))
+      .then((value) async {
+    final argParser = ArgParser()
+      ..addOption('target', abbr: 't', defaultsTo: TargetUrl)
+      ..addOption('port', abbr: 'p', defaultsTo: LocalPort.toString());
+    final argResults = argParser.parse(arguments);
+    final String targetUrl = argResults['target'];
+    final port = int.parse(argResults['port']);
+    final server = await shelf_io.serve(
+      proxyHandler(targetUrl),
+      LocalHost,
+      port,
+    );
+    configServer(server);
+    print('$targetUrl 的本地代理地址是 http://${server.address.host}:${server.port}');
+  }).catchError((error) {
+    print('An error occurred: $error');
+  });
+}
+
+Future<void> deleteFlutterWebToolsStamp() async {
+  try {
+    final flutterSdkPath = await getFlutterSdkPath();
+    if (flutterSdkPath != null) {
+      final flutterToolsStampPath =
+          '$flutterSdkPath/bin/cache/flutter_tools.stamp';
+      if (File(flutterToolsStampPath).existsSync()) {
+        await File(flutterToolsStampPath).delete();
+      }
+    } else {
+      throw Exception('Flutter SDK not found，please set FLUTTER_HOME.');
+    }
+  } catch (e) {
+    rethrow;
+  }
+}
+
+Future<bool> disableFlutterWebSecurity() async {
+  try {
+    final flutterSdkPath = await getFlutterSdkPath();
+    if (flutterSdkPath != null) {
+      final flutterChromePath =
+          '$flutterSdkPath/packages/flutter_tools/lib/src/web/chrome.dart';
+      if (File(flutterChromePath).existsSync()) {
+        final file = File(flutterChromePath);
+        var content = file.readAsStringSync();
+        final targetString = '\'--disable-extensions\'';
+        final insertString = '\'--disable-web-security\'';
+        if (content.contains(insertString)) {
+          // print('The chrome web security have been turned off.');
+          return false;
+        }
+        if (content.contains(targetString)) {
+          content = content.replaceFirst(
+              targetString, '$insertString,\n\t\t$targetString');
+          file.writeAsStringSync(content);
+          // print('The chrome web security have been turned off.');
+          return true;
+        } else {
+          throw Exception(
+              'The target string "$targetString" option was not found in $flutterChromePath.');
+        }
+      } else {
+        throw Exception(
+            '$flutterChromePath file not found or had been deleted.');
+      }
+    } else {
+      throw Exception('Flutter SDK not found，please set FLUTTER_HOME.');
+    }
+  } catch (e) {
+    rethrow;
+  }
+}
+
+//Building flutter tool...
+Future<void> reBuildFlutterTool(bool reBuildTolls) async {
+  if (!reBuildTolls) {
+    return;
+  }
+  var result = Process.runSync('flutter', ['doctor', '-v']);
+  final stdout = result.stdout;
+  final stderr = result.stderr;
+  if (result.exitCode != 0) {
+    throw ('flutter doctor -v failed:\n$stderr');
+  }
+  // print('Flutter doctor output:\n$stdout');
+}
+
+Future<String?> getFlutterSdkPath() async {
+  final flutterSdkPathEnv = Platform.environment['FLUTTER_HOME'];
+  if (flutterSdkPathEnv != null) {
+    return flutterSdkPathEnv;
+  } else {
+    final homeDir = Platform.environment['HOME'];
+    final userProfileDir = Platform.environment['USERPROFILE'];
+
+    if (homeDir != null) {
+      final flutterSdkPath = '$homeDir/flutter';
+      if (await Directory(flutterSdkPath).exists()) {
+        return flutterSdkPath;
+      }
+    }
+
+    if (userProfileDir != null) {
+      final flutterSdkPath = '$userProfileDir/flutter';
+      if (await Directory(flutterSdkPath).exists()) {
+        return flutterSdkPath;
+      }
+    }
+
+    return null;
+  }
 }
 
 void configServer(HttpServer server) {
@@ -39,7 +139,7 @@ void configServer(HttpServer server) {
   server.defaultResponseHeaders.add('Access-Control-Max-Age', '3600');
 }
 
-Handler customProxyHandler(
+Handler proxyHandler(
   url, {
   http.Client? client,
   String? proxyName,
